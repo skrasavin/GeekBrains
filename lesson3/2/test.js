0e90997d03b2024567fd97aa53b067dd8caa7c55
skrasavin <s-krasssavin@mail.ru>
2018-10-25 13:22:59 +0300

    add lesson3

--- lesson3/2/test.js
+++ lesson3/2/test.js
@@ -1,8 +1,8 @@
-var re = /[\']/;
+var re = /'\B|\B'/g;
 
-var text = "a'rent 's' 'asdasd'";
+var text = "They sing songs, 'dance', look at pictures 'in interesting' books, draw, " +
+    "learn to get on with 'their' classmates. She is Sebastian Marlow, aren't you?";
 
 text = text.replace(re, "\"");
-
-
 console.log(text);
+
---
+++ lesson3/3/test.js
@@ -0,0 +1,49 @@
+
+
+window.onload = function () {
+    let $check = document.getElementById("name");
+    let $phone = document.getElementById("phone");
+    let $email = document.getElementById("email");
+    let $message = document.getElementById("message");
+    let error = document.getElementById("errorText");
+    let errorPhone = document.getElementById("errorTextPhone");
+    let errorEmail = document.getElementById("errorTextEmail");
+    let errorMessage = document.getElementById("errorTextMessage");
+
+    let $button = document.getElementById("button").onclick = checkForm;
+
+    function checkForm() {
+        if (/[a-z]$/.test($check.value)) {
+            $check.style.borderColor = "green";
+            error.innerHTML = "\u0026#9989";
+        } else {
+            $check.style.borderColor = "red";
+            error.innerHTML = "&nbsp&nbsp&nbsp&nbsp Доступны для ввода только латинские буквы."
+        }
+
+        if (/\+7\(\d{3}\)\d{3}-\d{4}$/.test($phone.value)) {
+            $phone.style.borderColor = "green";
+            errorPhone.innerHTML = "\u0026#9989";
+        } else {
+            $phone.style.borderColor = "red";
+            errorPhone.innerHTML = "&nbsp&nbsp&nbsp&nbsp Формат ввода - +7(000)000-0000"
+        }
+        if (/(\w{6}|\w{2}.\w{4}|\w{2}-\w{4})@mail.ru$/.test($email.value)) {
+            $email.style.borderColor = "green";
+            errorEmail.innerHTML = "\u0026#9989";
+        } else {
+            $email.style.borderColor = "red";
+            errorEmail.innerHTML = "&nbsp&nbsp&nbsp&nbsp Формат ввода email - mymail@mail.ru, my-mail@mail.ru, " +
+                "my.mail@mail.ru"
+        }
+        if (!/^\s*$/.test($message.value)) {
+            $message.style.borderColor = "green";
+            errorMessage.innerHTML = "\u0026#9989";
+        }else {
+            $message.style.borderColor = "red";
+            errorMessage.innerHTML = "&nbsp&nbsp&nbsp&nbspТекст для ввода";
+        }
+
+
+    };
+};
